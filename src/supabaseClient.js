b6eec983c4aa8a709830064524afbf3d879307b6
AMIR <cidamir2@gmail.com>
2026-07-22 02:24:51 +0300

    update supabase client with direct keys

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://utlvhpxjykxigudmrpsm.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0bHZocHhqeWt4aWd1ZG1ycHNtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg4NDE5MTksImV4cCI6MjA1NDQxNzkxOX0.vP6m3bC5Zk1Wz7v2L5N4M9X8J1K2L3M4N5O6P7Q8R9S'
 
 if (!supabaseUrl || !supabaseAnonKey) {
   // eslint-disable-next-line no-console
