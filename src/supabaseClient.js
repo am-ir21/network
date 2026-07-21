db3338a6a700ab2cd76728fa1c782498c699f74c
AMIR <cidamir2@gmail.com>
2026-07-22 02:46:08 +0300

    restore original database keys

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://utlvhpajykxigudmrpsm.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0bHZocGFqeWt4aWd1ZG1ycHNtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM4ODQ1NTksImV4cCI6MjA5OTQ2MDU1OX0.tDzsugraIPx2GRxBFwFud7iuDDBdwQcuhfFs7vWUN0U'
+const supabaseUrl = 'https://bwfuldyqtdpyxlvizhvx.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ3ZnVsZHlxdGRweXhsdml6aHZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM5MzM4OTYsImV4cCI6MjA5OTUwOTg5Nn0.9qVp2ZiDwS8cA_QDEwMjrMgx3mxxoxNTwobPXlCuAJY'
 if (!supabaseUrl || !supabaseAnonKey) {
   // eslint-disable-next-line no-console
   console.warn(
