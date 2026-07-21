2920e3d0ed7a9d06e9564cfc9035ef3729aa60e7
AMIR <cidamir2@gmail.com>
2026-07-22 02:31:01 +0300

    fix correct url

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://utlvhpxjykxigudmrpsm.supabase.co'
+const supabaseUrl = 'https://utlvhpajykxigudmrpsm.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0bHZocHhqeWt4aWd1ZG1ycHNtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg4NDE5MTksImV4cCI6MjA1NDQxNzkxOX0.vP6m3bC5Zk1Wz7v2L5N4M9X8J1K2L3M4N5O6P7Q8R9S'
 
 if (!supabaseUrl || !supabaseAnonKey) {
